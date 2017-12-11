6124a5278008729cd52d2a840d8840ce372c7af7
asnid <adam.sniderman@gmail.com>
2017-12-11 14:41:08 -0500

    K-Means algorithm with scikitlearn

---
+++ ml9_kmeans.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+K-MEANS ALGORITHM using sklearn
+
+"""
+
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+import numpy as np
+from sklearn.cluster import KMeans
+
+X = np.array([[1, 2], [1.5, 1.8], [5, 8], [8, 8], [1, 0.6], [9, 11]])
+# plt.scatter(X[:,0], X[:,1], s=150, linewidths=5)
+# plt.show()
+
+
+# K-MEANS CLASSIFIER
+
+clf = KMeans(n_clusters=2)
+clf.fit(X)
+
+centroids = clf.cluster_centers_
+labels = clf.labels_
+
+# Plot data and cluster centroids
+
+colors = 10 * ['g.','r.','c.','b.']    # Green Red Cyan Blue Black Orange
+
+for i in range(len(X)):
+    plt.plot(X[i][0], X[i][1], colors[labels[i]], markersize=15)
+
+plt.scatter(centroids[:,0], centroids[:,1], marker='x', s=150, linewidths=5, c='k')
+plt.show()
